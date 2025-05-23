b019e1344e6872d9a49709e24faf12450310610b
Amrish-Amy <chauhanamrish32@gmail.com>
2025-05-23 20:24:04 +0530

    Updated the Home pge and Skillset page

--- src/components/Skillset/Techstack.jsx
+++ src/components/Skillset/Techstack.jsx
@@ -12,7 +12,9 @@ import {
   SiSolidity,
   SiRedux,
   SiMongodb,
-  SiHtml5
+  SiHtml5,
+  SiTailwindcss,
+  SiStyledcomponents
 } from "react-icons/si";
 
 const Techstack = () => {
@@ -20,33 +22,43 @@ const Techstack = () => {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiHtml5 />
+        <span className="tech-name">HTML5</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <span className="tech-name">JavaScript</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <span className="tech-name">Node JS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <span className="tech-name">React JS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRedux />
+        <span className="tech-name">Redux</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMongodb />
+        <span className="tech-name">MongoDB</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiGraphql />
+        <SiStyledcomponents />
+        <span className="tech-name">Styled Components</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
+        <SiTailwindcss />
+        <span className="tech-name">Tailwind CSS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMaterialui />
+        <span className="tech-name">Material UI</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <span className="tech-name">GIT</span>
       </Col>
     </Row>
   );
--- src/components/Skillset/Toolstack.jsx
+++ src/components/Skillset/Toolstack.jsx
@@ -13,18 +13,23 @@ const Toolstack = () => {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <span className="tech-name">VS Code</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <span className="tech-name">Postman</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
+        <span className="tech-name">Vercel</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNetlify />
+        <span className="tech-name">Netlify</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiHeroku />
+        <span className="tech-name">Heroku</span>
       </Col>
     </Row>
   );
