26d0bf7038fe9bb8c4609ccf6a9c10b54545182f
Amrish-Amy <chauhanamrish32@gmail.com>
2025-05-24 13:31:23 +0530

    Updated the Resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -10,6 +10,9 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
+const resumeLink = `https://amrish-portfolio.vercel.app/assets/2025-Amrish-Resume-BEcxosRM.pdf`
+
+
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
@@ -35,7 +38,7 @@ const Resume = () => {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
+          <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
